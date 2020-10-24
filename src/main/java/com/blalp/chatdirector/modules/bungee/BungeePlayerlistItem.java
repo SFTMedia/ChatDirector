5205e0b7f8f51eff0dc035d35d9bc8a4e805629e
blalp <26471229+blalp@users.noreply.github.com>
2020-10-24 11:34:53 -0700

    modified bungee playerlist

--- src/main/java/com/blalp/chatdirector/modules/bungee/BungeePlayerlistItem.java
+++ src/main/java/com/blalp/chatdirector/modules/bungee/BungeePlayerlistItem.java
@@ -1,33 +1,40 @@
 package com.blalp.chatdirector.modules.bungee;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import com.blalp.chatdirector.ChatDirector;
 import com.blalp.chatdirector.model.Item;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class BungeePlayerlistItem extends Item {
     public String triggerWord = "playerlist";
     public boolean ignoreCase = true;
     public String format = "```\nOnline players (%NUM_PLAYERS%/%MAX_PLAYERS%):\n";
     public String formatNoPlayers = "**No online players**";
+    public String formatPlayer = "%PLAYER_NAME%";
     @Override
     public String process(String string, Map<String,String> context) {
-        context.putAll(ChatDirector.formatter.getContext(Bukkit.getServer()));
+        context.putAll(ChatDirector.formatter.getContext(ProxyServer.getInstance()));
         if (!((string.equalsIgnoreCase(triggerWord)&&ignoreCase)||(string.equals(triggerWord)))) {
             return string;
         }
         String output = ChatDirector.formatter.format(format,context);
         boolean first = true;
-        for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+        Map<String,String> tempContext = new HashMap<>();
+        tempContext.putAll(context);
+        for (ProxiedPlayer player : ProxyServer.getInstance().getPlayers()) {
             if (!first) {
                 output += ", ";
             } else {
                 first = false;
             }
-            output += player.getName();
+            tempContext.putAll(ChatDirector.formatter.getContext(player));
+            output += ChatDirector.formatter.format(formatPlayer, tempContext);
+            tempContext= new HashMap<>();
+            tempContext.putAll(context);
         }
         output += "\n```";
         if (output.equals(ChatDirector.formatter.format(format.replace("%NUM_PLAYERS%", "0"),context))) {
