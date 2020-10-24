8ea6c410cff86022d608a0f037767c8ed3b32edd
blalp <26471229+blalp@users.noreply.github.com>
2020-10-24 11:50:23 -0700

    prepended server to server related stats

--- src/main/java/com/blalp/chatdirector/modules/sponge/SpongeFormatter.java
+++ src/main/java/com/blalp/chatdirector/modules/sponge/SpongeFormatter.java
@@ -12,9 +12,9 @@ public class SpongeFormatter extends Formatter {
     @Override
     public Map<String, String> getContext(Object event) {
         HashMap<String,String> context = new HashMap<>();
-        context.put("NUM_PLAYERS",String.valueOf(Sponge.getServer().getOnlinePlayers().size()));
-        context.put("MAX_PLAYERS",String.valueOf(Sponge.getServer().getMaxPlayers()));
-        context.put("MOTD",String.valueOf(Sponge.getServer().getMotd()));
+        context.put("SERVER_NUM_PLAYERS",String.valueOf(Sponge.getServer().getOnlinePlayers().size()));
+        context.put("SERVER_MAX_PLAYERS",String.valueOf(Sponge.getServer().getMaxPlayers()));
+        context.put("SERVER_MOTD",String.valueOf(Sponge.getServer().getMotd()));
         if(event instanceof Player) {
             context.put("PLAYER_NAME",((Player)event).getName());
             context.put("PLAYER_UUID",((Player)event).getUniqueId().toString());
