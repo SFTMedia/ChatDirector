ecbdceed5c05fac4dbe485614fef7e624bfab7fa
blalp <26471229+blalp@users.noreply.github.com>
2020-10-24 12:29:42 -0700

    upped version

--- src/main/java/com/blalp/chatdirector/ChatDirectorSponge.java
+++ src/main/java/com/blalp/chatdirector/ChatDirectorSponge.java
@@ -10,7 +10,7 @@ import org.spongepowered.api.event.message.MessageChannelEvent;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.plugin.Plugin;
 
-@Plugin(id="chatdirector",name = "Chat Director",version = "0.0.2-alpha",description = "Manages as much Chat as needed.")
+@Plugin(id="chatdirector",name = "Chat Director",version = "0.0.3-alpha",description = "Manages as much Chat as needed.")
 public class ChatDirectorSponge {
     private ChatDirector chatDirector;
 
