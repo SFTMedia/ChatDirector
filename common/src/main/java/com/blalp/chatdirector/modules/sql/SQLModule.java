35b89ff4f4d342cf72bd9756948b8db3ac4a9c38
blalp <26471229+blalp@users.noreply.github.com>
2020-10-28 15:12:27 -0700

    added table creation

--- common/src/main/java/com/blalp/chatdirector/modules/sql/SQLModule.java
+++ common/src/main/java/com/blalp/chatdirector/modules/sql/SQLModule.java
@@ -1,5 +1,6 @@
 package com.blalp.chatdirector.modules.sql;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -10,28 +11,34 @@ import com.blalp.chatdirector.model.IItem;
 import com.blalp.chatdirector.modules.Module;
 
 public class SQLModule extends Module {
-    public static HashMap<String,SQLConnection> connections = new HashMap<>();
+    public static HashMap<String, SQLConnection> connections = new HashMap<>();
+    public static HashMap<String, ArrayList<String>> tables = new HashMap<>();
 
-    public SQLModule(LinkedHashMap<String,LinkedHashMap<String,String>> map){
-        for (Entry<String,String> connectionEntry : map.get("connections").entrySet()) {
+    public SQLModule(LinkedHashMap<String, LinkedHashMap<String, String>> map) {
+        for (Entry<String, String> connectionEntry : map.get("connections").entrySet()) {
             SQLConnection connection = new SQLConnection(connectionEntry.getValue());
             connections.put(connectionEntry.getKey(), connection);
+            tables.put(connectionEntry.getKey(), new ArrayList<>());
         }
     }
 
     @Override
     public String[] getItemNames() {
-        return new String[]{"send-data","retrieve-data"};
+        return new String[] { "send-data", "retrieve-data" };
     }
 
     @Override
     public IItem createItem(String type, Object config) {
-        LinkedHashMap<String, String> configMap = (LinkedHashMap<String, String>)config;
-        switch(type){
+        LinkedHashMap<String, String> configMap = (LinkedHashMap<String, String>) config;
+        switch (type) {
             case "send-data":
-                return new SQLSendDataItem(configMap.get("table"),configMap.get("name"),configMap.get("key"),configMap.get("connection"),configMap.get("value"));
+                tables.get(configMap.get("connection")).add(configMap.get("table"));
+                return new SQLSendDataItem(configMap.get("table"), configMap.get("name"), configMap.get("key"),
+                        configMap.get("connection"), configMap.get("value"));
             case "retrieve-data":
-                return new SQLRetrieveDataItem(configMap.get("table"),configMap.get("name"),configMap.get("key"),configMap.get("connection"));
+                tables.get(configMap.get("connection")).add(configMap.get("table"));
+                return new SQLRetrieveDataItem(configMap.get("table"), configMap.get("name"), configMap.get("key"),
+                        configMap.get("connection"));
             default:
                 return null;
         }
@@ -39,8 +46,17 @@ public class SQLModule extends Module {
 
     @Override
     public void load() {
-        for(SQLConnection connection:connections.values()){
-            connection.load();
+        for (Entry<String, SQLConnection> connection : connections.entrySet()) {
+            connection.getValue().load();
+            for (String table : tables.get(connection.getKey())) {
+                try {
+                    connection.getValue().connection.prepareStatement(
+                            "CREATE TABLE IF NOT EXISTS ? (name varchar(255),key varchar(255), value varchar(255));")
+                            .execute();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
