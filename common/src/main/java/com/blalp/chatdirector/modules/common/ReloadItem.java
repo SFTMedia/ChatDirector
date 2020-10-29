9199080a97142cc1292cc72a9e8fc25f92494112
blalp <26471229+blalp@users.noreply.github.com>
2020-10-29 02:05:55 -0700

    added another failsafe

--- common/src/main/java/com/blalp/chatdirector/modules/common/ReloadItem.java
+++ common/src/main/java/com/blalp/chatdirector/modules/common/ReloadItem.java
@@ -3,13 +3,20 @@ package com.blalp.chatdirector.modules.common;
 import java.util.Map;
 
 import com.blalp.chatdirector.ChatDirector;
+import com.blalp.chatdirector.configuration.TimedLoad;
 import com.blalp.chatdirector.model.Item;
 
 public class ReloadItem extends Item {
 
     @Override
     public String process(String string, Map<String, String> context) {
-        ChatDirector.instance.reload();
+        try {
+            ChatDirector.instance.reload();
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("reload from command failed, triggering timed save");
+            new Thread(new TimedLoad()).start();
+        }
         return string;
     }
     
