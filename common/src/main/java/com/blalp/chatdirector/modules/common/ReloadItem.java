16a2017b4e6a165f8192dd3bebd1cb737a89c55c
blalp <26471229+blalp@users.noreply.github.com>
2020-11-13 06:53:12 -0800

    dont reload while reloading

--- common/src/main/java/com/blalp/chatdirector/modules/common/ReloadItem.java
+++ common/src/main/java/com/blalp/chatdirector/modules/common/ReloadItem.java
@@ -2,7 +2,6 @@ package com.blalp.chatdirector.modules.common;
 
 import java.util.Map;
 
-import com.blalp.chatdirector.ChatDirector;
 import com.blalp.chatdirector.configuration.TimedLoad;
 import com.blalp.chatdirector.model.Item;
 
@@ -10,13 +9,7 @@ public class ReloadItem extends Item {
 
     @Override
     public String process(String string, Map<String, String> context) {
-        try {
-            ChatDirector.instance.reload();
-        } catch (Exception e){
-            e.printStackTrace();
-            System.out.println("reload from command failed, triggering timed save");
-            new Thread(new TimedLoad()).start();
-        }
+        new Thread(new TimedLoad()).start();
         return string;
     }
     
