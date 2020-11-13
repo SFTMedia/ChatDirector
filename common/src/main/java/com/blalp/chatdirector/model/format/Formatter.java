fa58d4265cc73ad374fcf1bd38ef8e901705f3a2
blalp <26471229+blalp@users.noreply.github.com>
2020-11-13 06:55:44 -0800

    make sure context is not being modified

--- common/src/main/java/com/blalp/chatdirector/model/format/Formatter.java
+++ common/src/main/java/com/blalp/chatdirector/model/format/Formatter.java
@@ -1,6 +1,5 @@
 package com.blalp.chatdirector.model.format;
 
-import java.util.ConcurrentModificationException;
 import java.util.Map;
 import java.util.Map.Entry;
 public abstract class Formatter implements IFormatter {
@@ -10,9 +9,11 @@ public abstract class Formatter implements IFormatter {
         if(format==null){
             return null;
         }
-        for (Entry<String,String> singleContext : context.entrySet()) {
-            if(singleContext.getKey()!=null&singleContext.getValue()!=null){
-                format=format.replace("%"+singleContext.getKey()+"%", singleContext.getValue());
+        synchronized (context) {
+            for (Entry<String,String> singleContext : context.entrySet()) {
+                if(singleContext.getKey()!=null&singleContext.getValue()!=null){
+                    format=format.replace("%"+singleContext.getKey()+"%", singleContext.getValue());
+                }
             }
         }
         return format;
