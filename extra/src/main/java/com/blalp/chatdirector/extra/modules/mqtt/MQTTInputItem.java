26b4504a82e544dc6fb12c03eaaecde83c2f811a
blalp <26471229+blalp@users.noreply.github.com>
2021-05-21 05:31:55 -0400

    init mqtt commit

---
+++ extra/src/main/java/com/blalp/chatdirector/extra/modules/mqtt/MQTTInputItem.java
@@ -0,0 +1,16 @@
+package com.blalp.chatdirector.extra.modules.mqtt;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@JsonNaming(PropertyNamingStrategy.KebabCaseStrategy.class)
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class MQTTInputItem extends MQTTItem {
+    String topic;
+}
