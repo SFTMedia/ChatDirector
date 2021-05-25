791fa03913bcf5c1df2c45b618c26649e9819142
blalp <26471229+blalp@users.noreply.github.com>
2021-05-25 01:41:04 -0700

    make sure InputDeamon loads after DiscordBots

--- discord/src/main/java/com/blalp/chatdirector/modules/javacord/DiscordBots.java
+++ discord/src/main/java/com/blalp/chatdirector/modules/javacord/DiscordBots.java
@@ -7,22 +7,30 @@ import com.blalp.chatdirector.model.ILoadable;
 
 public class DiscordBots extends HashMap<String, DiscordBot> implements ILoadable {
 
+    boolean loaded=false;
+
     @Override
     public boolean load() {
-        for (Entry<String, String> bot : ChatDirector.getConfig().getModuleData().get("discord").entrySet()) {
-            if (!this.containsKey(bot.getKey())) {
-                this.put(bot.getKey(), new DiscordBot(bot.getValue()));
+        if(!loaded){
+            loaded=true;
+            for (Entry<String, String> bot : ChatDirector.getConfig().getModuleData().get("discord").entrySet()) {
+                if (!this.containsKey(bot.getKey())) {
+                    this.put(bot.getKey(), new DiscordBot(bot.getValue()));
+                }
             }
+            boolean result = true;
+            for (DiscordBot discordBot : this.values()) {
+                result = result && discordBot.load();
+            }
+            return result;
+        } else {
+            return true;
         }
-        boolean result = true;
-        for (DiscordBot discordBot : this.values()) {
-            result = result && discordBot.load();
-        }
-        return result;
     }
 
     @Override
     public boolean unload() {
+        loaded=false;
         boolean result = true;
         for (DiscordBot discordBot : this.values()) {
             result = result && discordBot.unload();
