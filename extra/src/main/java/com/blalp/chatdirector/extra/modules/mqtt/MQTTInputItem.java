14eb6a6628a656d3cad640265b3a7c6e542f1665
blalp <26471229+blalp@users.noreply.github.com>
2021-05-28 02:19:22 -0700

    make sure connections exist if output item does

--- extra/src/main/java/com/blalp/chatdirector/extra/modules/mqtt/MQTTInputItem.java
+++ extra/src/main/java/com/blalp/chatdirector/extra/modules/mqtt/MQTTInputItem.java
@@ -1,16 +1,20 @@
 package com.blalp.chatdirector.extra.modules.mqtt;
 
+import com.blalp.chatdirector.ChatDirector;
+import com.blalp.chatdirector.model.IDaemon;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 @JsonNaming(PropertyNamingStrategy.KebabCaseStrategy.class)
-@NoArgsConstructor
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class MQTTInputItem extends MQTTItem {
     String topic;
+
+    public MQTTInputItem() {
+        ((IDaemon) ChatDirector.getConfigStaging().getOrCreateDaemon(MQTTInputDaemon.class)).addItem(this);
+    }
 }
