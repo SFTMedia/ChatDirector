2ad4a3f934a8633805cf5e466a5a9c5be03557a0
blalp <26471229+blalp@users.noreply.github.com>
2021-05-28 02:17:38 -0700

    simplified discord module load

--- discord/src/main/java/com/blalp/chatdirector/modules/javacord/DiscordModule.java
+++ discord/src/main/java/com/blalp/chatdirector/modules/javacord/DiscordModule.java
@@ -23,15 +23,9 @@ public class DiscordModule implements IModule {
 
     @Override
     public boolean load() {
-        boolean result = true;
         // Make sure a DiscordBots item is created
         ChatDirector.getConfig().getOrCreateDaemon(DiscordBots.class);
-        if (ChatDirector.getConfig().getModuleData() == null
-                || !ChatDirector.getConfig().getModuleData().containsKey("discord")) {
-            System.err.println("discord module does not have any data");
-            return true;
-        }
-        return result;
+        return true;
     }
 
     @Override
