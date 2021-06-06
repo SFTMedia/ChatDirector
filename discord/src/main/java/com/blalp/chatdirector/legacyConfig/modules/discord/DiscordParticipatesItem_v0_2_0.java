87f1707e2dafd2de0c7fcf07312fc92fb2e0f271
blalp <26471229+blalp@users.noreply.github.com>
2021-06-05 22:29:16 -0700

    legacyConfig discord Chain to LegacyChain

--- discord/src/main/java/com/blalp/chatdirector/legacyConfig/modules/discord/DiscordParticipatesItem_v0_2_0.java
+++ discord/src/main/java/com/blalp/chatdirector/legacyConfig/modules/discord/DiscordParticipatesItem_v0_2_0.java
@@ -2,7 +2,7 @@ package com.blalp.chatdirector.legacyConfig.modules.discord;
 
 import java.util.List;
 
-import com.blalp.chatdirector.core.configuration.Chain;
+import com.blalp.chatdirector.core.configuration.LegacyChain;
 import com.blalp.chatdirector.core.model.ILegacyItem;
 import com.blalp.chatdirector.core.model.Version;
 
@@ -11,7 +11,7 @@ import lombok.Data;
 @Data
 public class DiscordParticipatesItem_v0_2_0 implements ILegacyItem {
     int length = 50;
-    Chain each;
+    LegacyChain each;
     String channel,bot;
     @Override
     public List<ILegacyItem> updateToNextLegacyItems() {
